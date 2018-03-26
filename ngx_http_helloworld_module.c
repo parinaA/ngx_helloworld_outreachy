a910bac7b1c3ddb68341501fefaf85e413f947b1
parinaA <parinaanand12@gmail.com>
2018-03-27 02:53:39 +0530

    updated file

--- ngx_http_helloworld_module.c
+++ ngx_http_helloworld_module.c
@@ -1,57 +1,55 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
-static char *ngx_http_hello
-world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+static char *ngx_http_helloworld(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t  ngx_http_helloworld_commands[] = {
-    { ngx_string("Hello World"),
-      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
-      ngx_http_helloworld,
-      NGX_HTTP_LOC_CONF_OFFSET,
-      0,
-      NULL },
 
-      ngx_null_command
+  { ngx_string("helloworld"),
+    NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+    ngx_http_helloworld,
+    0,
+    0,
+    NULL },
+
+    ngx_null_command
 };
+
+
+static u_char  ngx_helloworld[] = "hello world";
+
 static ngx_http_module_t  ngx_http_helloworld_module_ctx = {
-    NULL,                          /* preconfiguration */
-    NULL,                          /* postconfiguration */
+  NULL,                          /* preconfiguration */
+  NULL,                          /* postconfiguration */
 
-    NULL,                          /* create main configuration */
-    NULL,                          /* init main configuration */
+  NULL,                          /* create main configuration */
+  NULL,                          /* init main configuration */
 
-    NULL,                          /* create server configuration */
-    NULL,                          /* merge server configuration */
+  NULL,                          /* create server configuration */
+  NULL,                          /* merge server configuration */
 
-    ngx_http_helloworld_create_loc_conf,  /* create location configuration */
-    NULL/* merge location configuration */
+  NULL,                          /* create location configuration */
+  NULL                           /* merge location configuration */
 };
-static void *
-ngx_http_helloworld_create_loc_conf(ngx_conf_t *cf)
-{
-    ngx_http_helloworld_loc_conf_t  *conf;
 
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_helloworld_loc_conf_t));
-    if (conf == NULL) {
-        return NGX_CONF_ERROR;
-    }
-    return conf;
-}
-ngx_module_t  ngx_http_helloworld_module = {
-    NGX_MODULE_V1,
-    &ngx_http_helloworld_module_ctx, /* module context */
-    ngx_http_helloworld_commands,   /* module directives */
-    NGX_HTTP_MODULE,               /* module type */
-    NULL,                          /* init master */
-    NULL,                          /* init module */
-    NULL,                          /* init process */
-    NULL,                          /* init thread */
-    NULL,                          /* exit thread */
-    NULL,                          /* exit process */
-    NULL,                          /* exit master */
-    NGX_MODULE_V1_PADDING
+
+ngx_module_t ngx_http_helloworld_module = {
+  NGX_MODULE_V1,
+  &ngx_http_helloworld_module_ctx, /* module context */
+  ngx_http_helloworld_commands,   /* module directives */
+  NGX_HTTP_MODULE,               /* module type */
+  NULL,                          /* init master */
+  NULL,                          /* init module */
+  NULL,                          /* init process */
+  NULL,                          /* init thread */
+  NULL,                          /* exit thread */
+  NULL,                          /* exit process */
+  NULL,                          /* exit master */
+  NGX_MODULE_V1_PADDING
 };
+
+
 static ngx_int_t ngx_http_helloworld_handler(ngx_http_request_t *r)
 {
   ngx_buf_t    *b;
@@ -71,18 +69,19 @@ static ngx_int_t ngx_http_helloworld_handler(ngx_http_request_t *r)
   b->last_buf = 1;
 
   r->headers_out.status = NGX_HTTP_OK;
-  r->headers_out.content_length_n = sizeof(ngx_hello_world);
+  r->headers_out.content_length_n = sizeof(ngx_helloworld);
   ngx_http_send_header(r);
 
   return ngx_http_output_filter(r, &out);
 }
-static char *
-ngx_http_helloworld(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+
+
+static char *ngx_http_helloworld(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_http_core_loc_conf_t  *clcf;
+  ngx_http_core_loc_conf_t  *clcf;
 
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_helloworld_handler;
+  clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+  clcf->handler = ngx_http_helloworld_handler;
 
-    return NGX_CONF_OK;
+  return NGX_CONF_OK;
 }
